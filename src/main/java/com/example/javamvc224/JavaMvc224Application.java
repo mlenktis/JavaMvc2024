039604c479ff15fd18ae7adf1596affabb9c5b79
DESKTOP-PBSAHHN\Laptoop <n.lenktis@gmail.com>
2024-03-12 09:23:24 +0200

    init

---
+++ src/main/java/com/example/javamvc224/JavaMvc224Application.java
@@ -0,0 +1,13 @@
+package com.example.javamvc224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvc224Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMvc224Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamvc224/JavaMvc224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamvc224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMvc224ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
