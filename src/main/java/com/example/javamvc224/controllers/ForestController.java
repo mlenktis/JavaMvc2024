a761df659f2e76ea5b9efb4a56766dec21b3df28
DESKTOP-PBSAHHN\Laptoop <n.lenktis@gmail.com>
2024-03-12 11:44:14 +0200

    get data from API to BE

--- src/main/java/com/example/javamvc224/controllers/ForestController.java
+++ src/main/java/com/example/javamvc224/controllers/ForestController.java
@@ -5,14 +5,48 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @Controller
 public class ForestController {
+
     @GetMapping("/")
-    public ModelAndView index(){
+    public ModelAndView index() throws IOException {
         ModelAndView modelAndView = new ModelAndView("index");
 
+        var forecasts = getForecastModels();
+
+        modelAndView.addObject("forecasts", forecasts);
+        var text = getMeteoForecastJson();
+        System.out.println(text);
+
+        return modelAndView;
+    }
+    public static String getMeteoForecastJson() throws IOException {
+        URL url = new URL("https://api.meteo.lt/v1/places/vilnius/forecasts/long-term");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        String text = "";
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            text += scanner.nextLine();
+        }
+        scanner.close();
+        return text;
+
+
+    }
+
+    private static ArrayList<ForecastModel> getForecastModels() {
         var forecasts = new ArrayList<ForecastModel>();
         var row1 = new ForecastModel("2024-03-12 11:00", 1.0);
         var row2 = new ForecastModel("2024-03-12 12:00", 2.0);
@@ -21,9 +55,6 @@ public class ForestController {
         forecasts.add(row1);
         forecasts.add(row2);
         forecasts.add(row3);
-
-        modelAndView.addObject("forecasts", forecasts);
-
-        return modelAndView;
+        return forecasts;
     }
 }
